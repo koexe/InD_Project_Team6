fe079eae95f12bd7325882989241995208db332d
Nohs <153786970+N1oh@users.noreply.github.com>
2024-05-27 02:03:29 +0900

    05-27 노현성
    1. 다이어로그 추가
    2. 2차전 종료시 엔딩씬 이동

---
+++ Assets/Scripts/UI/TransferScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // 씬 관리를 위한 네임스페이스 추가
+
+public class TransferScene : MonoBehaviour
+{
+    public string targetSceneName; // 이동할 씬 이름
+
+    // 박스 콜라이더에 닿는 순간 이벤트 발생
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 씬 이동
+        SceneManager.LoadScene(targetSceneName);
+    }
+}
